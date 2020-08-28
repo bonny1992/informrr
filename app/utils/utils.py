ade8f0f75df4a96743117fb303f9b8b752b70186
Bonny1992 <bonny1992@gmail.com>
2020-08-28 19:00:58 +0200

    Switched our and source in print function

--- app/utils/utils.py
+++ app/utils/utils.py
@@ -2,9 +2,9 @@ import time
 
 def aprint(msg, source='MAIN'):
     now = time.ctime()
-    message = '{SOURCE}\t\t| {TIME} |\t\t{MSG}'
+    message = '{TIME}\t\t| {SOURCE} |\t\t{MSG}'
     print(message.format(
+        TIME = now,
         SOURCE = source,
-        MSG = msg,
-        TIME = now
+        MSG = msg
     ))
