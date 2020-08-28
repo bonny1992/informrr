fcba12138bf9e8b9cfe656a18ee33f7255f39b54
Bonny1992 <bonny1992@gmail.com>
2020-08-28 18:52:04 +0200

    Time wasn't written

--- app/utils/utils.py
+++ app/utils/utils.py
@@ -1,8 +1,8 @@
 import time
 
 def aprint(msg, source='MAIN'):
-    now = time.strftime('%l:%M%p %z on %b %d, %Y')
-    message = '{SOURCE}\t| {TIME} |\t{MSG}'
+    now = time.ctime()
+    message = '{SOURCE}\t\t| {TIME} |\t\t{MSG}'
     print(message.format(
         SOURCE = source,
         MSG = msg,
