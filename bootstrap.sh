520796785d0f12d0fc7c3edb27862309ee824d32
Bonny1992 <bonny1992@gmail.com>
2020-09-07 03:37:06 +0200

    Tested a bit of vagrant

--- bootstrap.sh
+++ bootstrap.sh
@@ -8,6 +8,24 @@ if ! [ -L /app ]; then
   ln -fs /vagrant/app /app
 fi
 
+if ! [ -L /data ]; then
+  sudo chown -R 1000:1000 /data
+else
+  sudo mkdir /data
+  sudo chown -R 1000:1000 /data
+fi
+
+
 cd /app
 sudo pip3 install -r requirements.txt
 sudo pip3 install pyngrok
+
+
+
+
+
+
+echo "Run these in tmux sessions:"
+echo "** tmux new -d -s ngrok ngrok http 5445 **"
+echo "** tmux new -d -s webhook python3 /app/main.py **"
+echo "** tmux new -d -s notify python3 /app/notificator.py **"
